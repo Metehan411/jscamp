8dd9b6d6a6f99c40f696caf7dbbd18366658a846
Metehan411 <orcenmetehan@gmail.com>
2021-10-24 01:02:32 +0300

    18 ekim ders sonu

---
+++ src/app.js
@@ -0,0 +1,32 @@
+console.log("merhaba ben")
+
+let dolardun = 9.20
+let dolarbugun = 9.30
+
+{
+    let dolardun = 9.10
+
+}
+
+
+console.log(dolardun)
+
+const eurodun = 11.2
+//eurodun = 11 bu hata const değeri değiştirilemez 
+
+console.log(eurodun)
+
+//array
+//camelCasing
+//PascalCasing
+let konutkredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+
+console.log("<ul>")
+for (let i = 0; i <konutkredileri.length;i++) {
+    console.log("<li>"+konutkredileri[i]+"</li>")
+    
+}
+console.log("</ul>")
+
+
+console.log(konutkredileri)
